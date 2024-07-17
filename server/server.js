9bd8706529061901d16799cfdb1c9a18dbdf0aab
Debanjan Banerjee <debanjanbanerjee456.gmail.com>
2024-07-17 14:41:49 +0530

    working model complete

--- server/server.js
+++ server/server.js
@@ -13,4 +13,21 @@ io.on("connection",(socket)=>{
         io.to(room).emit("user:joined", { email, id: socket.id });
         io.to(socket.id).emit('room:joined',data);
     })
+    socket.on("user:call", ({ to, offer }) => {
+      io.to(to).emit("incomming:call", { from: socket.id, offer });
+    });
+
+    socket.on("call:accepted", ({ to, ans }) => {
+      io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
+
+    socket.on("peer:nego:needed", ({ to, offer }) => {
+      console.log("peer:nego:needed", offer);
+      io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+    });
+
+    socket.on("peer:nego:done", ({ to, ans }) => {
+      console.log("peer:nego:done", ans);
+      io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+    });
 });
